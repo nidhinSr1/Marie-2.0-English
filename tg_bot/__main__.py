2e0c43fb17ed2ad94e3a6dd1bc5913b0a29086fe
nidhinSr1 <68446505+nidhinSr1@users.noreply.github.com>
2020-07-20 13:04:49 +0300

    Update __main__.py

--- tg_bot/__main__.py
+++ tg_bot/__main__.py
@@ -18,7 +18,7 @@ from tg_bot.modules.helper_funcs.chat_status import is_user_admin
 from tg_bot.modules.helper_funcs.misc import paginate_modules
 
 PM_START_TEXT = """
-Sorry.. This Bot Can Only Be Used in [Cinema Factory](https://t.me/Cf_chat) Group!
+Sorry.. This Bot Can Only Be Used in Cinema Factory Group!
 
 
 
